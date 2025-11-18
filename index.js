3cc2cff9b6596d893980e7e2ad849095f4c504ce
keerthana769 <54412620+keerthana769@users.noreply.github.com>
2025-11-18 15:53:27 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var source="./images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src", source);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var source="./images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src", source);
+
+if (randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player 1 Won";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Won🚩";
+}
+else if(randomNumber1===randomNumber2){
+    document.querySelector("h1").innerHTML="Draw!";
+}
+
